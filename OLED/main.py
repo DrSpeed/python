e550b80c53d64e3644088dcae057e64528a4870f
Dr Speed <doctor_speed_2001@yahoo.com>
2022-12-27 13:45:56 -0800

    Update main.py
    Comment

--- OLED/main.py
+++ OLED/main.py
@@ -4,6 +4,7 @@ import adafruit_ssd1306
 import busio as io
 
 
+# Adafruit M0 Feather
 # Initialize I2C and OLED device
 i2c = io.I2C(board.SCL, board.SDA)
 oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3D)
